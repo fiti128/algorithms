908c0900b45048fc96e9f6897e7ba4f8d9109c72
siarhei.yanusheuski <tr-legres@yandex.ru>
2019-09-20 07:47:59 -0400

    Updated with first implementation

---
+++ src/task/easy/MaximumSubarray.java
@@ -0,0 +1,24 @@
+package task.easy;
+
+/**
+ * @author Sergey Yanushevskiy
+ */
+public class MaximumSubarray {
+    public int maxSubArray(int[] nums) {
+        int largestSum = nums[0];
+        int tempSum = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            if (tempSum + nums[i] > tempSum && (tempSum + nums[i] > nums[i])) {
+                tempSum = tempSum + nums[i];
+            } else {
+                tempSum = nums[i];
+            }
+            largestSum = tempSum > largestSum
+                    ? tempSum
+                    : largestSum;
+
+
+        }
+        return largestSum;
+    }
+}
--- src/task/easy/TwoSum.java
+++ src/task/easy/TwoSum.java
@@ -1,4 +1,4 @@
-package task;
+package task.easy;
 
 import java.util.Arrays;
 import java.util.HashMap;
